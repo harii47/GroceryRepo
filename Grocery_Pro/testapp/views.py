63fd4508979586db08d530759ead5abbb370f9ae
harii47 <52786843+harii47@users.noreply.github.com>
2021-01-28 10:35:33 +0530

    Update views.py

--- Grocery_Pro/testapp/views.py
+++ Grocery_Pro/testapp/views.py
@@ -1,12 +1,25 @@
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from testapp.models import Grocery_Item_Model, User_Model,Bought_Item
+from testapp.models import Grocery_Item_Model, User_Model, Bought_Item
 import re
 
 all_item = Grocery_Item_Model.objects.all()
 
 
 def home(request):
+    if request.method == "POST":
+        date_filter = request.POST.get('date')
+        print(type(date_filter))
+        filter = Grocery_Item_Model.objects.filter(date__day=date_filter[8:10], date__month=date_filter[5:7],
+                                                   date__year=date_filter[0:4])
+        try:
+            usr = request.session['user']
+            print(usr)
+            user = User_Model.objects.get(email=usr)
+        except:
+            return render(request, 'index.html', {'all_item': filter})
+        return render(request, 'index.html', {'all_item': filter, 'loggedin_user': user})
     items = Grocery_Item_Model.objects.all()
     try:
         usr = request.session['user']
@@ -15,7 +28,7 @@ def home(request):
     except:
         return render(request, 'index.html', {'all_item': items})
     # Bought_Item.objects.get(user.id
-    return render(request, 'index.html', {'all_item': items, 'user':user})
+    return render(request, 'index.html', {'all_item': items, 'loggedin_user': user})
 
 
 def register(request):
@@ -26,7 +39,6 @@ def register(request):
         typo = "Registration"
         if (re.findall('^[9|8|7|6]', mobile)) == "9" or '8' or '7' or '6':
             if len(mobile) >= 10:
-                request.session['user'] = email
                 password = request.POST.get('password')
                 if len(password) < 5:
                     reply = "Invalid Password please enter valid password"
@@ -39,6 +51,7 @@ def register(request):
                     except User_Model.DoesNotExist:
                         address = request.POST.get('address')
                         User_Model(name=name, mobile=mobile, email=email, password=password, address=address).save()
+                        request.session['user'] = str(email)
                         return redirect('/home')
                     else:
                         msg = 'The Given Email Id Is Already Registered with User '
@@ -101,4 +114,19 @@ def select_item(request):
         return redirect('/home')
     print(type(user_id))
     item = Grocery_Item_Model.objects.get(id=user_id)
-    return render(request, 'add.html',{'all_item':item})
+    return render(request, 'add.html', {'all_item': item})
+
+
+def saved_item(request):
+    try:
+        user_id = request.session['user']
+    except:
+        return HttpResponse("Wait some more Time")
+    user_id = User_Model.objects.get(email=user_id)
+    items = Bought_Item.objects.filter(user_id=user_id.id)
+    item_list = []
+    for i in items:
+        item_list.append(i.grocery_item_id)
+        print(i.grocery_item_id, 'status')
+    item = Grocery_Item_Model.objects.filter(bought_item__user_id__in=item_list)
+    return render(request, 'index.html', {'all_item': item, 'loggedin_user': user_id, 'type': 'checking_list'})
